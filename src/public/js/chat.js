224b7435923be81d82e0c48aa7a44c26ae06d5d0
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-02-12 17:44:51 -0500

    feat: Configuring websockets with Socket.io and the events

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,2 +1,22 @@
-console.log('Hola');
-io();
+$(function () {
+
+  const socket = io();
+
+  // DOM elements
+  const $messageForm = $('#message-form');
+  const $messageBox = $('#message');
+  const $chat = $('#chat');
+
+  // emit events
+  $messageForm.submit(e => {
+    e.preventDefault();
+    socket.emit('client-send:message', $messageBox.val());
+    $messageBox.val('');
+  });
+
+  // listen events
+  socket.on('server-send:message', data => {
+    $chat.append(data + '<br>');
+  })
+  
+})
--- src/sockets.js
+++ src/sockets.js
@@ -1,5 +1,11 @@
 module.exports = function (io) {
+
   io.on('connect', (socket) => {
     console.log('New connection', socket.id);
+
+    socket.on('client-send:message', data => {
+      io.sockets.emit('server-send:message', data);
+    })
   });
+
 }
