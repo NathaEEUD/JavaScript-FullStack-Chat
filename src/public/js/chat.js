c719aa8fbf380a0205ecacf39c5783634fcfb307
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-02-12 21:37:59 -0500

    feat: Implementing the functionality of the chat application

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -2,21 +2,61 @@ $(function () {
 
   const socket = io();
 
-  // DOM elements
+  // DOM elements nickname form
+  const $nickForm = $('#nickForm');
+  const $nickname = $('#nickname');
+  const $nickError = $('#nickError');
+
+  // DOM elements chat
   const $messageForm = $('#message-form');
   const $messageBox = $('#message');
   const $chat = $('#chat');
 
+  // DOM elements users list
+  const $usernames = $('#usernames');
+
   // emit events
+  $nickForm.submit(e => {
+    e.preventDefault();
+    socket.emit('client-new:user', $nickname.val(), data => {
+      if (data) {
+        $('#nickWrap').hide();
+        $('#contentWrap').show();
+      } else {
+        $nickError.html(`
+          <div class="alert alert-danger">
+            That username already exits
+          </div>
+        `);
+      }
+      $nickname.val('');
+    });
+  });
+
   $messageForm.submit(e => {
     e.preventDefault();
-    socket.emit('client-send:message', $messageBox.val());
+    socket.emit('client-send:message', $messageBox.val(), data => {
+      $chat.append(`<p class="error">${data}</p>`);
+    });
     $messageBox.val('');
   });
 
   // listen events
+  socket.on('server-usernames', data => {
+    let html = '';
+    for (let i = 0; i < data.length; i++) {
+      html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`
+    }
+
+    $usernames.html(html);
+  });
+
   socket.on('server-send:message', data => {
-    $chat.append(data + '<br>');
-  })
+    $chat.append('<b>' + data.nick + '</b>: ' + data.msg + '<br/>');
+  });
+
+  socket.on('server-whisper', data => {
+    $chat.append(`<p class="whisper"><b>${data.nick}</b>: ${data.msg}</p>`);
+  });
   
 })
--- src/sockets.js
+++ src/sockets.js
@@ -1,11 +1,63 @@
 module.exports = function (io) {
 
+  let users = {};
+
   io.on('connect', (socket) => {
     console.log('New connection', socket.id);
 
-    socket.on('client-send:message', data => {
-      io.sockets.emit('server-send:message', data);
+    socket.on('client-new:user', (data, cb) => {
+      if (data in users) {
+        cb(false);
+      } else {
+        cb(true);
+        socket.nickname = data;
+        users[socket.nickname] = socket;
+        updateNicknames();
+      }
     })
+
+    socket.on('client-send:message', (data, cb) => {
+      let msg = data.trim();
+      if(msg.substr(0, 3) === '/w ') {
+        // private message
+        msg = msg.substr(3);
+        const whiteIndex = msg.indexOf(' ');
+
+        if(whiteIndex !== -1) {
+          let name = msg.substr(0, whiteIndex);
+          msg = msg.substr(whiteIndex + 1);
+
+          if(name in users) {
+            users[name].emit('server-whisper', {
+              msg,
+              nick: socket.nickname
+            });
+          } else {
+            cb('Error! Please enter a valid user.');
+          }
+        } else {
+          cb('Error! Please enter your message.')
+        }
+      } else {
+        // public message
+        io.sockets.emit('server-send:message', {
+          msg: data,
+          nick: socket.nickname
+        });
+      }
+
+    })
+
+    socket.on('disconnect', data => {
+      if (!socket.nickname) return;
+      delete users[socket.nickname];
+      updateNicknames();
+    })
+
+    function updateNicknames() {
+      // Object.keys(users) to assemble a keys array
+      io.sockets.emit('server-usernames', Object.keys(users));
+    }
   });
 
 }
